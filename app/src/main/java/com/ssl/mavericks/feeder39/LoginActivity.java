b38a396e8f09ffd5dc4b7eb41388ff31b2792649
Varshith <varshiths@cse.iitb.ac.in>
2016-10-26 14:34:55 +0530

    Implemented list view. Basic function calls implemented.

--- app/src/main/java/com/ssl/mavericks/feeder39/LoginActivity.java
+++ app/src/main/java/com/ssl/mavericks/feeder39/LoginActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,16 +16,47 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button b = (Button) findViewById(R.id.lButton);
+        if(false) {// if already logged in, authenticate again though
+            // get cred from storage
+            authenticate("Stored", "Stored");
 
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+            Toast.makeText(getApplicationContext(), R.string.prev_login_message, Toast.LENGTH_LONG);
+        }else{
 
-                startActivity(intent);
-            }
-        });
+            final EditText credentials = (EditText) findViewById(R.id.lUser);
+            final EditText password = (EditText) findViewById(R.id.lPassword);
 
+            Button loginButton = (Button) findViewById(R.id.lButton);
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String credStr = credentials.getText().toString();
+                    String passStr = password.getText().toString();
+
+                    authenticate(credStr, passStr);
+                }
+            });
+
+            TextView registerButton = (TextView) findViewById(R.id.lRegister);
+            registerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("Register Activity opens");
+                }
+            });
+        }
+
+
+    }
+
+    public void authenticate(String credStr, String passStr){
+        System.out.println("Authenticating '" + credStr + "' with password '" + passStr + "'");
+
+        if(true){
+            Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(getApplicationContext(), R.string.login_fail_string, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/ssl/mavericks/feeder39/UserActivity.java
+++ app/src/main/java/com/ssl/mavericks/feeder39/UserActivity.java
@@ -3,6 +3,7 @@ package com.ssl.mavericks.feeder39;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,10 +13,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
+
+import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.StringTokenizer;
 
 public class UserActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    ViewFlipper vf;
+    ListView courseList;
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +42,31 @@ public class UserActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Thread syncer = new Thread() {
+            public void run () {
+                for (;;) {
+                    syncData();
+                    try {
+                        float x = (float) 0.25; // x minutes
+                        Thread.sleep((long) (x * 60 * 1000));    // sleep for 3 seconds
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        });
+        };
+
+        vf = (ViewFlipper) findViewById(R.id.content_flipper);
+        courseList = (ListView) findViewById(R.id.courses_list);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -41,6 +76,38 @@ public class UserActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendar, caldroidFragment);
+        t.commit();
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        courseList.setAdapter(adapter);
+
+        courseList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,int position, long arg3)
+            {
+                openCourseActivity(position);
+            }
+        });
+        populateApp();
+
+        // all the initial population is here
+
+        syncData(); // initial sync
+
+        vf.setDisplayedChild(1);
+//        syncer.start();
+
     }
 
     @Override
@@ -60,20 +127,20 @@ public class UserActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -81,19 +148,33 @@ public class UserActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        ViewFlipper vf = (ViewFlipper) findViewById(R.id.content_flipper);
-
         if (id == R.id.nav_calendar) {
-            // Handle the camera action
+            vf.setDisplayedChild(1);
 
         } else if (id == R.id.nav_courses) {
+            vf.setDisplayedChild(2);
 
         } else if (id == R.id.nav_sync) {
-
+            syncData();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void syncData(){
+        System.out.println("Sync button pressed");
+    }
+
+    public void populateApp(){
+        listItems.add("This");
+        listItems.add("That");
+        listItems.add("What");
+        adapter.notifyDataSetChanged();
+    }
+
+    public void openCourseActivity(int index) {
+        Toast.makeText(getApplicationContext(), "" + index, Toast.LENGTH_SHORT).show();
     }
 }

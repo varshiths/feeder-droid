3233ea5dc23b2ecac8b67f070d7ce79aedf2be09
Varshith <varshiths@cse.iitb.ac.in>
2016-10-26 11:43:14 +0530

    Basic Layout with some redundant components

---
+++ app/src/main/java/com/ssl/mavericks/feeder39/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.ssl.mavericks.feeder39;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button b = (Button) findViewById(R.id.lButton);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+}
